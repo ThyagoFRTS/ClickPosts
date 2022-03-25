fe8aa8821611b11c3591cffba80847c9b8338276
ThyagoKZKR <kzkr.thyago@gmail.com>
2022-03-25 10:06:14 -0300

    add initial configs and global types

---
+++ src/global/types/posts.ts
@@ -0,0 +1,5 @@
+export interface PostsProps {
+    userId: number,
+    title: string,
+    body: string,
+}
---
+++ src/global/types/user.ts
@@ -0,0 +1,28 @@
+interface GeolocalizationProps {
+    lat: string,
+    lng: string,
+}
+
+interface AddressProps {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: GeolocalizationProps,
+}
+
+interface CompanyProps {
+    name: string,
+    catchPhrase: string,
+    bs: string,
+}
+
+export interface UserProps {
+    id: number,
+    name: string,
+    email: string,
+    phone: string,
+    website: string,
+    address: AddressProps,
+    company: CompanyProps,
+}
