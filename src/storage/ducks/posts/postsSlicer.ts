99313be3fdcc7094020f38ae7b40a054397fb9db
ThyagoKZKR <kzkr.thyago@gmail.com>
2022-03-29 14:45:21 -0300

    add id state manager on redux

--- src/storage/ducks/posts/postsSlicer.ts
+++ src/storage/ducks/posts/postsSlicer.ts
@@ -1,5 +1,6 @@
+import { useAppDispatch } from './../../../hooks/redux';
 import { PostProps } from './../../../global/types/post';
-import { AppDispatch } from './../../index';
+import store, { AppDispatch } from './../../index';
 import { PostsState } from './types';
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { api } from '../../../services/api';
@@ -15,26 +16,36 @@ type NewPost = {
 const INITIAL_STATE: PostsState = {
     posts: [],
     isLoading: true,
+    lastPostIndex: 0,
 }
 
 export const getPostsFromApi = () => async (dispatch: AppDispatch) => {
     const posts = await api.get<PostProps[]>('/posts')
         .then(response => response.data) as PostProps[]
     posts.sort(sortPosts)
-    dispatch(setPosts({ posts, isLoading: false }))
+    dispatch(setPosts({ posts, isLoading: false, lastPostIndex: posts[0].id }))
 }
 
 export const createPost = (newPost: NewPost) => async (dispatch: AppDispatch) => {
     const headers = { 'Content-type': 'application/json; charset=UTF-8' };
-    const post = await api.post<PostProps[]>('/posts', newPost, {headers})
-        .then(response => response.data) as PostProps
-    dispatch(addPost(post))
+    const post = await api.post<PostProps>('/posts', newPost, { headers })
+        .then(response => {
+            if (response.status == 201) {
+                return response.data
+            }
+        })
+    if (post) {
+        dispatch(updateLastPostIndex())
+        dispatch(addPost(post))
+    }
 }
 
 export const updatePost = (newPost: NewPost) => async (dispatch: AppDispatch) => {
     const headers = { 'Content-type': 'application/json; charset=UTF-8' };
-    const post = await api.post<PostProps[]>('/posts', newPost, {headers})
-        .then(response => response.data) as PostProps
+    const post = await api.post<PostProps[]>('/posts', newPost, { headers })
+        .then(response => {
+            return response.data
+        }) as PostProps
     dispatch(addPost(post))
 }
 
@@ -42,7 +53,7 @@ export const deletePost = (itemId: number) => async (dispatch: AppDispatch) => {
     const status = await api.delete<PostProps[]>(`/posts/${itemId}`)
         .then(response => response.status) as PostProps
     console.log(status)
-    dispatch(deletPost(itemId))
+    dispatch(removePost(itemId))
 }
 
 const postsSlice = createSlice({
@@ -52,16 +63,21 @@ const postsSlice = createSlice({
         setPosts: (state, action: PayloadAction<PostsState>) => {
             state.posts = action.payload.posts
             state.isLoading = action.payload.isLoading
+            state.lastPostIndex = action.payload.lastPostIndex
+        },
+        updateLastPostIndex: (state) => {
+            state.lastPostIndex += 1
         },
         addPost: (state, action: PayloadAction<PostProps>) => {
-            state.posts = [{...action.payload}].concat(state.posts) 
+            console.log({ ...action.payload, id: state.lastPostIndex })
+            state.posts = [{ ...action.payload, id: state.lastPostIndex }].concat(state.posts)
         },
-        deletPost: (state, action: PayloadAction<number>) => {
-            state.posts =state.posts.filter((item) => item.id != action.payload)
+        removePost: (state, action: PayloadAction<number>) => {
+            state.posts = state.posts.filter((item) => item.id != action.payload)
         }
     },
 
 })
 
-export const { setPosts, addPost, deletPost } = postsSlice.actions;
+export const { setPosts, addPost, removePost, updateLastPostIndex } = postsSlice.actions;
 export default postsSlice.reducer
--- src/storage/ducks/posts/types.ts
+++ src/storage/ducks/posts/types.ts
@@ -3,4 +3,5 @@ import { PostProps } from "../../../global/types/post"
 export type PostsState = {
     posts: PostProps[],
     isLoading: boolean,
+    lastPostIndex: number,
 }
